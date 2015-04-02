9c7b22b8bc89ab43e969f8854f480cd796f3dd92
Michael <ccUser@cc-notebook-19.home>
2015-04-01 22:00:45 -0400

    Debugging weird thing with time zones. Need to see what happens once everything is on heroku server :/

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,8 @@
 class FlightsController < ApplicationController
 
  def index
-    @flights = Journey.includes(:trip, flights: [:legs]).where("date(time_queried) = current_date")
+    #@flights = Journey.includes(:trip, flights: [:legs]).where("date(time_queried at time zone 'EST') >= current_date")
+    @flights = Journey.includes(:trip, flights: [:legs]).where("date(time_queried) >= current_date")
   end
 
 end
