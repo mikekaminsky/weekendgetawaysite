18839b53629dca2584522734fab895e48ceb940b
Michael <ccUser@cc-notebook-19.home>
2015-04-05 18:10:35 -0400

    Cleanup

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,7 +1,6 @@
 class FlightsController < ApplicationController
 
  def index
-    #@flights = Journey.includes(:trip, flights: [:legs]).where("date(time_queried at time zone 'EST') >= current_date")
     @flights = Journey.includes(:trip, flights: [:legs]).where("date(time_queried) >= current_date")
   end
 
