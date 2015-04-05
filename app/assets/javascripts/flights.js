bf8ed02cdc2d5e4fddc5ec39bd20a160021fa52c
Michael <ccUser@cc-notebook-19.home>
2015-04-05 18:11:16 -0400

    Linking to kayak on row click

--- app/assets/javascripts/flights.js
+++ app/assets/javascripts/flights.js
@@ -1,3 +1,16 @@
 jQuery(function() {
-  return $('#flights_table').dataTable();
+  table = $('#flights_table').dataTable({
+        "paging":   false,
+        "order": [[ 5, "asc" ]]
+    });
+
+  $('#flights_table').delegate('tbody > tr', 'click', function ()
+  {
+      // 'this' refers to the current <td>, if you need information out of it
+      var data = table.fnGetData(this);
+      url = 'https://www.kayak.com/flights/'+data[0]+'-'+data[1]+'/weekend'
+      window.open(url);
+  });
+
+
 });
